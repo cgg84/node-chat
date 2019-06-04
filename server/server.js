2993728bb5fbabb565a6e66b9f2670ee07a8d1d4
Chen <cgrinberg@gmail.com>
2019-06-04 07:56:38 +0300

    emit new message on create message

--- server/server.js
+++ server/server.js
@@ -19,15 +19,14 @@ io.on('connection', (socket) => {
         console.log('client disconnected');
     });
 
-    socket.emit('newMessage', {
-        from: 'chen',
-        text: 'Hey dude!',
-        createAt: 123
-    });
-
     socket.on('createMessage', (newMessage) => {
         console.log('createMessage => ', newMessage);
-    })
+        io.emit('newMessage', {
+            from: newMessage.from,
+            text: newMessage.text,
+            createdAt: new Date().getTime()
+        });
+    });
 });
 
 
